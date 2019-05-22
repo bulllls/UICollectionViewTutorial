2607c38b2cf50b3e67ac3491bd82343e7a8e109c
andrey <andrew@wearemad.ru>
2019-05-22 13:11:47 +0400

    Initial Commit

---
+++ CollectionViewTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewTutorial
+//
+//  Created by Андрей Зубехин on 22/05/2019.
+//  Copyright © 2019 MAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
